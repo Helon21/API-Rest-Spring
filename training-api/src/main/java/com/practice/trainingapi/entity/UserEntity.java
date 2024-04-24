394ef93f48cd81dcc4b806b75c1d3aa41fbd435a
Helon21 <helonbentes51@hotmail.com>
2024-04-24 12:40:22 -0300

    create UserRepository

--- training-api/src/main/java/com/practice/trainingapi/entity/UserEntity.java
+++ training-api/src/main/java/com/practice/trainingapi/entity/UserEntity.java
@@ -1,10 +1,23 @@
 package com.practice.trainingapi.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class UserEntity {
 
+    @Id
+    private Long id;
     private String name;
     private String email;
 
+    public UserEntity() {
+
+    }
+
+    public UserEntity(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
